38e5eb561cc1af9c897fb3d61bbf490485ec94d1
jithunmadhav <jithunmadhav1@gmail.com>
2023-07-31 15:03:31 +0530

    error handled

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
  
 
 const instance = axios.create({
-    // baseURL:'https://url-9uyl.onrender.com/',
-    baseURL:'http://localhost:4000/',
+    baseURL:'https://url-9uyl.onrender.com/',
+    // baseURL:'http://localhost:4000/',
 
   });
 
